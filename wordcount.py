58044549cb2cf7c16432aaafd7534537ecf4f7cc
Jerry Majors Patterson <jem@apple.com>
2021-10-27 03:26:19 +0000

    Switch to Using collections.Counter

--- wordcount.py
+++ wordcount.py
@@ -1,32 +1,20 @@
 import sys
+import re
+from collections import Counter
+
 
 def word_counts(path):
     """Count words in file.
     parameter: a path with strings
     prints word and its word count"""
 
-    text_to_count = open(path)
-    punctuation_marks = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
+    words = re.findall(r'\w+', open(path).read().lower())
+    words_dict_items = Counter(words).items()
+    for each_tuple in words_dict_items:
+        print(f"{each_tuple[0]}\t{each_tuple[1]}")
 
-    word_counts = {}
-
-    for line in text_to_count:
-        line = line.rstrip().split(" ")
-
-        for word in line:
-            for mark in punctuation_marks:
-                word = word.replace(mark, "")
-            word = word.lower()
-            word_counts[word] = word_counts.get(word, 0) + 1
-
-    for key, item in word_counts.items():
-        print(key, item)
-    print(f"\nThat was {len(word_counts)} different words.")
+    print(f"\nThat was {len(words_dict_items)} different words.")
 
 
-# path = "test.txt"
-# word_counts(path)
-
-# path = "twain.txt"
 path = sys.argv[1]
 word_counts(path)
