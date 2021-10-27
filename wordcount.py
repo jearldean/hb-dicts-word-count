ea986c50bf5305301b25183dd21a01831366e07b
Jerry Majors Patterson <jem@apple.com>
2021-10-27 03:58:33 +0000

    Got 3 kinds of sort working

--- wordcount.py
+++ wordcount.py
@@ -3,19 +3,29 @@ import re
 from collections import Counter
 
 
-def word_counts(path):
+def word_counts(path, sort_type='1'):
     """Count words in file.
     parameter: a path with strings
     prints word and its word count"""
 
     words = re.findall(r'\w+', open(path).read().lower())
     words_dict_items = Counter(words).items()
-    
-    for key, value in sorted(words_dict_items):
-        print(f"{key}\t{value}")
+
+    if sort_type == '1':  # sort by keys
+        for key, value in sorted(words_dict_items):
+            print(f"{key}\t{value}")
+    elif sort_type == '2':  # sort by values ascending
+        sort_dict = dict(sorted(words_dict_items, key=lambda x:x[1]))
+        for key, value in sort_dict.items():
+            print(f"{key}\t{value}")
+    else:   # sort by values descending
+        sort_dict = dict(sorted(words_dict_items, key=lambda x:-x[1]))
+        for key, value in sort_dict.items():
+            print(f"{key}\t{value}")
 
     print(f"\nThat was {len(words_dict_items)} different words.")
 
 
 path = sys.argv[1]
-word_counts(path)
+sort_type = sys.argv[2]
+word_counts(path, sort_type)
