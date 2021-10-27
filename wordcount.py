f77f58e27b29327f9e9c4b5311d07c6ca1f9a747
Jerry Majors Patterson <jem@apple.com>
2021-10-27 03:03:05 +0000

    Incorporated some of the Further Study Topics

--- wordcount.py
+++ wordcount.py
@@ -1,9 +1,12 @@
+import sys
+
 def word_counts(path):
     """Count words in file.
     parameter: a path with strings
     prints word and its word count"""
 
     text_to_count = open(path)
+    punctuation_marks = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
 
     word_counts = {}
 
@@ -11,11 +14,19 @@ def word_counts(path):
         line = line.rstrip().split(" ")
 
         for word in line:
+            for mark in punctuation_marks:
+                word = word.replace(mark, "")
+            word = word.lower()
             word_counts[word] = word_counts.get(word, 0) + 1
 
     for key, item in word_counts.items():
         print(key, item)
+    print(f"\nThat was {len(word_counts)} different words.")
 
 
-path = "test.txt"
+# path = "test.txt"
+# word_counts(path)
+
+# path = "twain.txt"
+path = sys.argv[1]
 word_counts(path)
